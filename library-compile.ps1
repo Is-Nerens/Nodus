81e39a68bf46987f56ad12640b69bdf619699f98
NoodlePlexium <arran.ns@icloud.com>
2026-01-06 13:27:56 +0200

    Organised project stucture to better suit github

---
+++ library-compile.ps1
@@ -0,0 +1,32 @@
+$headersInclude = "headers"
+$sdlLib = "src\thirdParty\SDL3\lib" 
+$sdlInclude = "src\thirdParty\SDL3\include"
+$glewInclude = "src\thirdParty\glew\include"
+$glewLib = "src\thirdParty\glew\lib"
+$freetypeInclude = "src\thirdParty\freetype\include"
+$freetypeLib = "src\thirdParty\freetype\lib"
+clang -std=c99 -O3 "nodus_library.c" `
+-I"$headersInclude" `
+-I"$glewInclude" `
+-I"$sdlInclude" `
+-I"$freetypeInclude" `
+-L"$glewLib" `
+-L"$sdlLib" `
+-L"$freetypeLib" `
+-lglew32 -lSDL3 -lopengl32 -lgdi32 -lfreetype `
+-shared `
+-o "nodus\lib\nodus.dll" -Wno-deprecated-declarations
+Remove-Item "nodus\lib\nodus.exp"
+
+# Replace old nodus.dll with updated one
+$sourceDll = "nodus\lib\nodus.dll"
+$destDll = "build\nodus.dll"
+if (Test-Path $sourceDll) {
+    if (Test-Path $destDll) {
+        Remove-Item $destDll -Force
+    }
+    Copy-Item $sourceDll $destDll -Force
+    Write-Host "✅ Updated nodus.dll in build folder."
+} else {
+    Write-Host "❌ nodus.dll not found in $sourceDll"
+}

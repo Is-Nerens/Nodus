5343074089648e4561165eaf5d67ecb3242b178e
NoodlePlexium <arran.ns@icloud.com>
2026-01-07 22:41:10 +0200

    Fixed bugs in the xml generate tree function

--- compile.ps1
+++ compile.ps1
@@ -16,6 +16,8 @@ clang -std=c99 -O3 "src\nodus_library.c" `
 -lglew32 -lSDL3 -lopengl32 -lgdi32 -lfreetype `
 -shared `
 -o "nodus\lib\nodus.dll" -Wno-deprecated-declarations
+Remove-Item nodus\lib\nodus.exp -Force
+
 
 # Replace old nodus.dll with updated one
 $sourceDll = "nodus\lib\nodus.dll"
